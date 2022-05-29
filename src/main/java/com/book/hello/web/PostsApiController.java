27cf84f2daec6b097c009a8b3d9182a1fd953f47
DESKTOP-G23PEU4\Obzen <njkang@obzen.com>
2022-05-29 23:09:28 +0900

    jpa complete

---
+++ src/main/java/com/book/hello/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.book.hello.web;
+
+import com.book.hello.service.PostsService;
+import com.book.hello.web.dto.PostsResponseDto;
+import com.book.hello.web.dto.PostsSaveRequestDto;
+import com.book.hello.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    //@PutMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id)    {
+        return postsService.findById(id);
+    }
+}
